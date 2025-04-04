b2ca4e074c4ec1807b177c639def6098c6bfb94a
zeus2opera <113746886+zeus2opera@users.noreply.github.com>
2025-04-05 02:33:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + " "
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        if not search_on:
+            search_on = fruit_chosen
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
