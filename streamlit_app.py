ca75dfb1884f444dd63501ef4b0b7b9b7ae9a139
zeus2opera <113746886+zeus2opera@users.noreply.github.com>
2025-04-05 02:11:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -42,3 +43,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
     
+# New section to display smoothiefroot nutrition info
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
