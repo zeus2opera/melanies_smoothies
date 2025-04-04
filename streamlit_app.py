d92fab99c293e9bf49052c2d4cec9483d40ccbfd
zeus2opera <113746886+zeus2opera@users.noreply.github.com>
2025-04-05 02:12:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,5 @@ if ingredients_list:
     
 # New section to display smoothiefroot nutrition info
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response)
+st_df = st.dataframe(data=smoothiefroot_response.json())
